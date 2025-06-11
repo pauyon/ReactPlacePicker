5c6875f0106b744df974341702063f19d1e34f38
Paulino Auyon <pauyon@jgboswell.com>
2025-06-11 09:06:40 -0700

    Use UseEffect to sort locations once

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
+import {sortPlacesByDistance} from './loc.js';
 
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
+  const [availablePlaces, setAvailablePlaces] = useState([]);
   const [pickedPlaces, setPickedPlaces] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) =>{
+    const sortedPlaces = sortPlacesByDistance(AVAILABLE_PLACES, position.coords.latitude, position.coords.longitude);
+    setAvailablePlaces(sortedPlaces);
+  });
+  }, []); // Empty dependency array ensures this runs only once after the initial render, excluding it makes it run on every render.
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -63,7 +72,8 @@ function App() {
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          places={availablePlaces}
+          fallbackText="Sorting places by distance..."
           onSelectPlace={handleSelectPlace}
         />
       </main>
